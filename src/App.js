82028cb67214beabc13e4a16a1d6fb15f902fa84
monicafidalgo <fidalgo@evident.nl>
2021-02-20 19:25:20 +0000

    Add footer and favicon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/monicafidalgo/" target="_blank" rel="noreferrer">Mónica Fidalgo</a> and is open-sourced on <a href="https://github.com/MonicaFidalgo/react-weather-app" target="_blank" rel="noreferrer">Github</a>
+      </footer>
     </div>
   );
 }
